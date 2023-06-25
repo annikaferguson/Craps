b21c5d9088d96be54deae5d2734488372345e9ab
annikaferguson <fergusak@mail.uc.edu>
2023-06-25 17:18:57 -0400

    final

--- src/Craps.java
+++ src/Craps.java
@@ -1,12 +1,10 @@
 import java.util.Random;
+import java.util.Scanner;
+
 public class Craps {
     public static void main(String[] args) {
         Random rnd = new Random();
-        int rndBirthMonthOff = rnd.nextInt(12); //generates random value 0-11
-        int rndBirthMonth = rnd.nextInt(12) + 1; // 1-12 by adding one
-
-        int dieOff = rnd.nextInt(6); // generates a random int between 0 and 5
-        int die = rnd.nextInt(6) + 1; // generates a random int between 1 and 5
+        Scanner in = new Scanner(System.in);
 
         int die1 = rnd.nextInt(6) + 1; // generates random int between 0-5
         int die2 = rnd.nextInt(6) + 1; // generates random int between 1-6
@@ -18,29 +16,31 @@ public class Craps {
         String playAgain = ""; //prompt user to play?
 
         System.out.println("Do you want to play? Input Yes or No");
+        playAgain = in.nextLine();
 
-            do {
+            if(playAgain.equalsIgnoreCase("yes"))
+            {
                 System.out.println("Die 1: " + die1);
                 System.out.println("Die 2: " + die2);
                 System.out.println("Total value of roll: " + crapsRoll);
 
-                if(crapsRoll = 2 | 3 | 12)
+                if(crapsRoll == 2 | crapsRoll == 3 | crapsRoll2 == 12)
                 {
-                    System.out.println("You have 'craps'! You have 'crapped out'! The game is over. You lost.);
-                } else if(crapsRoll = 7 | 11) {
+                    System.out.println("You have 'craps'! You have 'crapped out'! The game is over. You lost.");
+                } else if(crapsRoll == 7 | crapsRoll == 11) {
                     System.out.println("Your roll was a 'natural'! You win! Good job!");
                 } else {
-                    System.out.println("The point is: " + crapsRoll + "Keep rolling.");
-                    if(crapsRoll2 = crapsRoll)
+                    System.out.println("The point is: " + crapsRoll + " Keep rolling.");
+                    if(crapsRoll2 == crapsRoll)
                     {
                         System.out.println("You rolled: " + crapsRoll2 + "You made the point! You win!");
-                    } else if(crapsRoll2 = 7){
+                    } else if(crapsRoll2 == 7){
                         System.out.println("You rolled a 7! You lose.");
                     } else {
-                        System.out.println("You rolled: " + crapsRoll2 + "Keep rolling.");
+                        System.out.println("You rolled: " + crapsRoll2 + " Keep rolling.");
                     }
                 }
-            } while(playAgain.equalsIgnoreCase("yes"));
+            }
 
         System.out.println("Thanks for playing!");
     }
